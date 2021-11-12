0626cf2b8561b5c44646c8b7419880f927b1fe94
AdrianRivas24 <92918172+AdrianRivas24@users.noreply.github.com>
2021-11-12 12:11:41 +0100

    Update Arrays.java

--- T4/codigos2122/T4_Arrays/src/Arrays.java
+++ T4/codigos2122/T4_Arrays/src/Arrays.java
@@ -1,26 +1,31 @@
-
 public class Arrays {
 	
 	public static void main (String[]args) {
 		
-		// array de numeros
-		int[] numeros = new int[10];
-		boolean[] aciertos = new boolean[5];
-		String[] palabras = new String[3];
-		double[] decimales = new double [2];
+		//meter en un array de 15 posiciones, 15 numeros aleatorios entre el 0 - 50
+		int [] numero = new int [15];
 		
-		int [] numeroDos = new int[]{1,2,3,4,5,6,7,8,9,10};
-		int longitudArray = numeroDos.length;
-		
-		//System.out.println(longitudArray);
-		//System.out.println(numeroDos[9]);
-		
-		//sacar todos los valores
-		for (int i=0; i <= numeroDos.length - 1; i++) {
-			System.out.println(numeroDos[i]);
+		for (int i = 0; i < 15; i++) {
+			numero [i] = (int)(Math.random()*51);
+			System.out.println(numero);
 		}
 		
-		
+	//una vez generados los numeros indicar cuantos pares e impares hay en la coleccion
+		int pares = 0, impares = 0;
+		for (int i = 0; i < 15; i++) {
+			if (numero[i] %2 == 0) {
+				System.out.println("El numero es par");
+			}
+				
+			else {
+					System.out.println("El numero es impar");		
+			}
+				
+		}
+	
 	}
 
 }
+
+    
+
