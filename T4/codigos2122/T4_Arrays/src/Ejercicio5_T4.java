b69464a1bdef073b891047f0e69b480c377c96be
AdrianRivas24 <92918172+AdrianRivas24@users.noreply.github.com>
2021-11-18 10:27:14 +0100

    upload

---
+++ T4/codigos2122/T4_Arrays/src/Ejercicio5_T4.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Ejercicio7_T4 {
+	
+	public static void main (String[]args) {
+		Scanner lecturaTeclado = new Scanner(System.in);
+		
+		lecturaTeclado.close();
+	}
+
+}
