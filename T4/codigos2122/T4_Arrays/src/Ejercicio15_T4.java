d4f012a4962c0dbb41c88a1f1452617aa495ceaa
AdrianRivas24 <92918172+AdrianRivas24@users.noreply.github.com>
2021-11-22 11:43:54 +0100

    upload

--- T4/codigos2122/T4_Arrays/src/Ejercicio15_T4.java
+++ T4/codigos2122/T4_Arrays/src/Ejercicio15_T4.java
@@ -22,8 +22,11 @@ public class Ejercicio15_T4 {
 		alumnos[2][3] = 2;
 		alumnos[2][4] = true;
 		
-		int nota, asignaturas;
-		double notaMedia = nota/asignaturas;
+		String nombre = alumnos[0][0];
+		double media = ((double)((int)alumnos[0][1] + (int)alumnos[0][2] + (int)alumnos[0][3])/3);
+		boolean conv = (boolean)(alumnos[0][alumnos[0].length-1]);
+		System.out.printf("%s tiene una media de %.2f y tiene asignaturas convalidadas", nombre, media);
+		System.out.printf("%s tiene una media de %.2f y no tiene asignaturas convalidadas", nombre, media);
 		
 	}
 
